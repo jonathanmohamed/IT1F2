7d8f90a96e4fc06b2607c28e4000155fa9d58a5d
IsaacSem <47461659+IsaacSem@users.noreply.github.com>
2019-04-17 16:43:59 +0200

    jobs and legal social links

--- legal.php
+++ legal.php
@@ -81,16 +81,16 @@
             <div class="footer">
                 <div class="footerleft1"> 
                     <div class="footerimg1">
-                        <a class="footerimg" href="https://twitter.com"><img src="images/tweet.png" alt="twitter"/></a>
+                        <a class="footerimg" href="https://twitter.com/Arcadia70151932"><img src="images/tweet.png" alt="twitter"/></a>
                     </div>
                     <div class="footerimg1">
-                        <a class="footerimg" href="https://facebook.com"><img src="images/facebook.png" alt="facebook"/></a>
+                        <a class="footerimg" href="https://www.facebook.com/ArcadiaUniversityAT/"><img src="images/facebook.png" alt="facebook"/></a>
                     </div>
                     <div class="footerimg1">
-                        <a class="footerimg" href="https://youtube.com"><img src="images/youtube.png" alt="youtube"/></a>
+                        <a class="footerimg" href="https://www.youtube.com/?gl=NL"><img src="images/youtube.png" alt="youtube"/></a>
                     </div>
                     <div class="footerimg4">
-                        <a class="footerimg" href="https://instagram.com"><img src="images/instagram.png" alt="instagram"/></a>
+                        <a class="footerimg" href="https://www.instagram.com/arcadiauniversityat/"><img src="images/instagram.png" alt="instagram"/></a>
                     </div>												      
                 </div>		
                 <div class="footerright"> 
@@ -107,12 +107,12 @@
                 <div class="footerleft2 en"> 
                     <p><a href="jobs.php">&nbsp;&nbsp;Jobs&nbsp;&nbsp;</a>
                         <a href="legal.php">&nbsp;Legal Notices&nbsp;&nbsp;</a>
-                        <a href="privacy.php">&nbsp;&nbsp;Privacy Notices</a></p>
+                        
                 </div>
                 <div class="footerleft2 nl"> 
                     <p><a href="jobs.php">&nbsp;&nbsp;Banen&nbsp;&nbsp;</a>
                         <a href="legal.php">&nbsp;Wettelijke mededelingen&nbsp;&nbsp;</a>
-                        <a href="privacy.php">&nbsp;&nbsp;Privacykennisgevingen</a></p>
+                        
                 </div>
             </div>			
         </div>
