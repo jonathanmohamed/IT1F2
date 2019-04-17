8a9f2bfc343e3119b9a0fd0d11a1144872ca1111
Thomas Koops <thomas_koops@hotmail.com>
2019-04-17 10:48:18 +0200

    new template for header with added language and changed css to fit it

---
+++ templateHeader.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Arcadia University of Arts and Technology</title>
+        <meta charset="UTF-8">
+        <link href="style.css" type="text/css" rel="stylesheet"/>
+        <style>
+            <?php
+            if (isset($_POST['submit'])) {
+                $language = $_POST['language'];
+            } else {
+                $language = "en";
+            }
+            ?>
+            .nl
+            {
+                <?php
+                if ($language == "en") {
+                    echo'display:none;'
+                    . 'visibility: hidden;';
+                }
+                ?>
+            }   
+            .en
+            {
+                <?php
+                if ($language == "nl") {
+                    echo'display:none;'
+                    . 'visibility: hidden;';
+                }
+                ?>
+            }  
+        </style>
+    </head>
+    <body>
+        <div id="container">
+            <div class="header">
+                <div class="logo">
+                    <a href="index.html"><img src="images/logo.jpg" alt="Logo" class="logo-img"/></a>
+                </div>
+                <div class="buttonlist1">
+                    <a href="news.html" class="button"><h3>News</h3></a>
+                    <a href="studStory.html" class="button"><h3>Student Stories</h3></a>
+                    <a href="contact.php" class="button"><h3>Contact</h3></a>
+                    <div class="button">
+                        <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
+                            <select name="language">
+                                <option value="en" selected="selected">EN</option>
+                                <option value="nl">NL</option>
+                            </select>
+                            <input type="submit" name="submit" value="submit"/>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!--code here-->
+        </div>
+    </body>
+</html>
