2a6223b15b4b91a3e65050ec4df4caea0ee4c985
Thomas Koops <thomas_koops@hotmail.com>
2019-04-17 14:29:44 +0200

    translated legal

--- legal.php
+++ legal.php
@@ -1,71 +1,118 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>Arcadia University of Arts and Technology</title>
-    <meta charset="UTF-8">
-<<<<<<< HEAD:legal.php
-		<link href="CSS/style.css" type="text/css" rel="stylesheet"/>
-		<link href="CSS/legal.css" type="text/css" rel="stylesheet"/>
-=======
-		<link href="style.css" type="text/css" rel="stylesheet"/>
-		<link href="legal.css" type="text/css" rel="stylesheet"/>
->>>>>>> 4123aacc5b973aaff8c7300b7bb2e5fcc8b64588:legal.html
-  </head>
-  
-  <body>
-	<div id="container">
-	
-		<div class="header">
-			<div class="logo">
-				<a href="index.html"><img src="images/logo.jpg" alt="Logo" class="logo-img"/></a>
-			</div>
-			<div class="buttonlist1">
-				<a href="news.php" class="button"><h3>News</h3></a>
-				<a href="studStory.html" class="button"><h3>Student Stories</h3></a>
-				<a href="contact.php" class="button"><h3>Contact</h3></a>
-			</div>
-		</div>
-	<!-- your code here -->	
-		<div class="legal">
-			<h1>Legal Notices</h1>
-				<p>Although every effort has been taken to ensure the accuracy of the information on the website, we cannot guarantee that the content and the information contained in it is always up-to-date, complete or true. Accordingly, no rights can be derived from the contents of the website. Neither can any rights be derived from information offered on websites accessed through links on the Arcadia University or Arts and Science,(Arcadia UAS) website.</p>
-		 </div>
-	
-	
-
-	<!-- your code here -->			
-		<div class="footer">
-			<div class="footerleft1"> 
-				<div class="footerimg1">
-					<a id="footerimg" href="https://twitter.com"><img src="images/tweet.png" alt="twitter"/></a>
-				</div>
-				<div class="footerimg1">
-					<a id="footerimg" href="https://facebook.com"><img src="images/facebook.png" alt="facebook"/></a>
-				</div>
-				<div class="footerimg1">
-					<a id="footerimg" href="https://youtube.com"><img src="images/youtube.png" alt="youtube"/></a>
-				</div>
-				<div class="footerimg4">
-					<a id="footerimg" href="https://instagram.com"><img src="images/instagram.png" alt="instagram"/></a>
-				</div>												      
-				</div>		
-		<div class="footerright"> 
-				<div class="footerimg2">
-						<a id="footerimg" href="https://www.acbsp.org/"><img src="images/acbsp.png" alt="acbsp"/></a>
-				</div>
-				<div class="footerimg2">
-						<a id="footerimg" href="https://www.accaglobal.com"><img src="images/acca.png" alt="acca"/></a>
-				</div>
-				<div class="footerimg2">
-						<a id="footerimg" href="https://www.nvao.net"><img src="images/nvao.png" alt="nvao"/></a>
-				</div>
-			</div>
-			<div class="footerleft2"> 
-				<p><a href="jobs.html">&nbsp;&nbsp;Jobs&nbsp;&nbsp;</a>
-				<a href="legal.html">&nbsp;Legal Notices&nbsp;&nbsp;</a>
-				<a href="privacy.html">&nbsp;&nbsp;Privacy Notices</a></p>
-			</div>
-		</div>			
-	</div>
-  </body>
+<html lang="en">
+    <head>
+        <title>Arcadia University of Arts and Technology</title>
+        <meta charset="UTF-8">
+        <link href="CSS/style.css" type="text/css" rel="stylesheet"/>
+        <link href="CSS/legal.css" type="text/css" rel="stylesheet"/>
+        <style>
+            <?php
+            if (isset($_POST['submit'])) {
+                $language = $_POST['language'];
+            } else {
+                $language = "en";
+            }
+            ?>
+            .nl
+            {
+                <?php
+                if ($language == "en") {
+                    echo'display:none;'
+                    . 'visibility: hidden;';
+                }
+                ?>
+            }   
+            .en
+            {
+                <?php
+                if ($language == "nl") {
+                    echo'display:none;'
+                    . 'visibility: hidden;';
+                }
+                ?>
+            }  
+        </style>
+    </head>
+    <body>
+        <div id="container">
+            <div class="header">
+                <div class="logo">
+                    <a href="index.php"><img src="images/logo.jpg" alt="Logo" class="logo-img"/></a>
+                </div>
+                <div class="buttonlist1 en">
+                    <a href="news.html" class="button"><h3 class="en">News</h3></a>
+                    <a href="studStory.php" class="button"><h3 class="en">Student Stories</h3></a>
+                    <a href="contact.php" class="button"><h3 class="en">Contact</h3></a>
+                    <div class="button">
+                        <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
+                            <select name="language">
+                                <option value="en" selected="selected">EN</option>
+                                <option value="nl">NL</option>
+                            </select>
+                            <input type="submit" name="submit" value="submit"/>
+                        </form>
+                    </div>
+                </div>
+                <div class="buttonlist1 nl">
+                    <a href="news.php" class="button"><h3>Nieuws</h3></a>
+                    <a href="studStory.php" class="button"><h3>Belevingen Studenten</h3></a>
+                    <a href="contact.php" class="button"><h3>Contact</h3></a>
+                    <div class="button">
+                        <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
+                            <select name="language">
+                                <option value="en">EN</option>
+                                <option value="nl" selected="selected">NL</option>
+                            </select>
+                            <input type="submit" name="submit" value="submit"/>
+                        </form>
+                    </div>
+                </div>
+            </div>	
+            <div class="legal en">
+                <h1>Legal Notices</h1>
+                <p>Although every effort has been taken to ensure the accuracy of the information on the website, we cannot guarantee that the content and the information contained in it is always up-to-date, complete or true. Accordingly, no rights can be derived from the contents of the website. Neither can any rights be derived from information offered on websites accessed through links on the Arcadia University of Arts and Science,(Arcadia UAS) website.</p>
+            </div>
+            <div class="legal nl">
+                <h1>Wettelijke mededelingen</h1>
+                <p>Alhoewel moeite is gedaan om te zorgen dat alle informatie op deze website accuraat is, kunnen we niet guaranderen dat alle informatie op deze website altijd correct bijgewerkt, compleet of waar is. Dus, geen rechten kunnen worden afgelegd van de informatie geleverd via links op de Arcadia Hogeschool van Kunst en Wetenschap, (Arcadia UAS) website.</p>
+            </div>
+            <div class="footer">
+                <div class="footerleft1"> 
+                    <div class="footerimg1">
+                        <a class="footerimg" href="https://twitter.com"><img src="images/tweet.png" alt="twitter"/></a>
+                    </div>
+                    <div class="footerimg1">
+                        <a class="footerimg" href="https://facebook.com"><img src="images/facebook.png" alt="facebook"/></a>
+                    </div>
+                    <div class="footerimg1">
+                        <a class="footerimg" href="https://youtube.com"><img src="images/youtube.png" alt="youtube"/></a>
+                    </div>
+                    <div class="footerimg4">
+                        <a class="footerimg" href="https://instagram.com"><img src="images/instagram.png" alt="instagram"/></a>
+                    </div>												      
+                </div>		
+                <div class="footerright"> 
+                    <div class="footerimg2">
+                        <a class="footerimg" href="https://www.acbsp.org/"><img src="images/acbsp.png" alt="acbsp"/></a>
+                    </div>
+                    <div class="footerimg2">
+                        <a class="footerimg" href="https://www.accaglobal.com"><img src="images/acca.png" alt="acca"/></a>
+                    </div>
+                    <div class="footerimg2">
+                        <a class="footerimg" href="https://www.nvao.net"><img src="images/nvao.png" alt="nvao"/></a>
+                    </div>
+                </div>
+                <div class="footerleft2 en"> 
+                    <p><a href="jobs.php">&nbsp;&nbsp;Jobs&nbsp;&nbsp;</a>
+                        <a href="legal.php">&nbsp;Legal Notices&nbsp;&nbsp;</a>
+                        <a href="privacy.php">&nbsp;&nbsp;Privacy Notices</a></p>
+                </div>
+                <div class="footerleft2 nl"> 
+                    <p><a href="jobs.php">&nbsp;&nbsp;Banen&nbsp;&nbsp;</a>
+                        <a href="legal.php">&nbsp;Wettelijke mededelingen&nbsp;&nbsp;</a>
+                        <a href="privacy.php">&nbsp;&nbsp;Privacykennisgevingen</a></p>
+                </div>
+            </div>			
+        </div>
+    </body>
 </html>
