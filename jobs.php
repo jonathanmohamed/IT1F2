5b7ed94ddc4153d9e3947e9771265949c2919905
Thomas Koops <thomas_koops@hotmail.com>
2019-04-17 14:54:29 +0200

    Added translations to jobs and legal

--- jobs.php
+++ jobs.php
@@ -1,71 +1,70 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Arcadia University of Arts and Technology</title>
-    <meta charset="UTF-8">
-<<<<<<< HEAD:jobs.php
-		<link href="CSS/style.css" type="text/css" rel="stylesheet"/>
-		<link href="CSS/jobs.css" type="text/css" rel="stylesheet"/>
-=======
-		<link href="style.css" type="text/css" rel="stylesheet"/>
-		<link href="jobs.css" type="text/css" rel="stylesheet"/>
->>>>>>> 4123aacc5b973aaff8c7300b7bb2e5fcc8b64588:jobs.html
-  </head>
-  
-  <body>
-	<div id="container">
-	
-		<div class="header">
-			<div class="logo">
-				<a href="index.html"><img src="images/logo.jpg" alt="Logo" class="logo-img"/></a>
-			</div>
-			<div class="buttonlist1">
-				<a href="news.php" class="button"><h3>News</h3></a>
-				<a href="studStory.html" class="button"><h3>Student Stories</h3></a>
-				<a href="contact.php" class="button"><h3>Contact</h3></a>
-			</div>
-		</div>
-	<!-- your code here -->	
-		<div class="vacancies">
-			<h1>Vacancies and internships</h1>
-				<p>Are you looking for a challenging work environment in which you can make the difference? Come and work at NHL Stenden University of Applied Sciences.</p>
-		 </div>
-	
-	
+    <head>
+        <title>Arcadia University of Arts and Technology</title>
+        <meta charset="UTF-8">
+        <link href="CSS/style.css" type="text/css" rel="stylesheet"/>
+        <link href="CSS/jobs.css" type="text/css" rel="stylesheet"/>
+    </head>
 
-	<!-- your code here -->			
-		<div class="footer">
-			<div class="footerleft1"> 
-				<div class="footerimg1">
-					<a id="footerimg" href="https://twitter.com"><img src="images/tweet.png" alt="twitter"/></a>
-				</div>
-				<div class="footerimg1">
-					<a id="footerimg" href="https://facebook.com"><img src="images/facebook.png" alt="facebook"/></a>
-				</div>
-				<div class="footerimg1">
-					<a id="footerimg" href="https://youtube.com"><img src="images/youtube.png" alt="youtube"/></a>
-				</div>
-				<div class="footerimg4">
-					<a id="footerimg" href="https://instagram.com"><img src="images/instagram.png" alt="instagram"/></a>
-				</div>												      
-				</div>		
-		<div class="footerright"> 
-				<div class="footerimg2">
-						<a id="footerimg" href="https://www.acbsp.org/"><img src="images/acbsp.png" alt="acbsp"/></a>
-				</div>
-				<div class="footerimg2">
-						<a id="footerimg" href="https://www.accaglobal.com"><img src="images/acca.png" alt="acca"/></a>
-				</div>
-				<div class="footerimg2">
-						<a id="footerimg" href="https://www.nvao.net"><img src="images/nvao.png" alt="nvao"/></a>
-				</div>
-			</div>
-			<div class="footerleft2"> 
-				<p><a href="jobs.html">&nbsp;&nbsp;Jobs&nbsp;&nbsp;</a>
-				<a href="legal.html">&nbsp;Legal Notices&nbsp;&nbsp;</a>
-				<a href="privacy.html">&nbsp;&nbsp;Privacy Notices</a></p>
-			</div>
-		</div>			
-	</div>
-  </body>
+    <body>
+        <div id="container">
+
+            <div class="header">
+                <div class="logo">
+                    <a href="index.php"><img src="images/logo.jpg" alt="Logo" class="logo-img"/></a>
+                </div>
+                <div class="buttonlist1">
+                    <a href="news.php" class="button"><h3>News</h3></a>
+                    <a href="studStory.php" class="button"><h3>Student Stories</h3></a>
+                    <a href="contact.php" class="button"><h3>Contact</h3></a>
+                </div>
+            </div>
+            <!-- your code here -->	
+            <div class="vacancies en" id="blueLink">
+                <h1>Vacancies and internships</h1>
+                <p>Are you looking for a challenging work environment in which you can make the difference? Come and work at Arcadia University of Arts and Science. To contact us <a href="contact.php">click here</a></p>
+            </div>
+            <div class="vacancies nl" id="blueLink">
+                <h1>Vactures en stagiaire</h1>
+                <p>Ben je opzoek naar een uitdagende werkomgeving waarin je een verschil kan maken? Kom werken bij Arcadia Hogeschool van Kunst en Wetenschap. Om in contact to komen <a href="contact.php">Klik hier</a></p>
+            </div>
+
+
+
+            <!-- your code here -->			
+            <div class="footer">
+                <div class="footerleft1"> 
+                    <div class="footerimg1">
+                        <a id="footerimg" href="https://twitter.com"><img src="images/tweet.png" alt="twitter"/></a>
+                    </div>
+                    <div class="footerimg1">
+                        <a id="footerimg" href="https://facebook.com"><img src="images/facebook.png" alt="facebook"/></a>
+                    </div>
+                    <div class="footerimg1">
+                        <a id="footerimg" href="https://youtube.com"><img src="images/youtube.png" alt="youtube"/></a>
+                    </div>
+                    <div class="footerimg4">
+                        <a id="footerimg" href="https://instagram.com"><img src="images/instagram.png" alt="instagram"/></a>
+                    </div>												      
+                </div>		
+                <div class="footerright"> 
+                    <div class="footerimg2">
+                        <a id="footerimg" href="https://www.acbsp.org/"><img src="images/acbsp.png" alt="acbsp"/></a>
+                    </div>
+                    <div class="footerimg2">
+                        <a id="footerimg" href="https://www.accaglobal.com"><img src="images/acca.png" alt="acca"/></a>
+                    </div>
+                    <div class="footerimg2">
+                        <a id="footerimg" href="https://www.nvao.net"><img src="images/nvao.png" alt="nvao"/></a>
+                    </div>
+                </div>
+                <div class="footerleft2"> 
+                    <p><a href="jobs.php">&nbsp;&nbsp;Jobs&nbsp;&nbsp;</a>
+                        <a href="legal.php">&nbsp;Legal Notices&nbsp;&nbsp;</a>
+                        <a href="privacy.php">&nbsp;&nbsp;Privacy Notices</a></p>
+                </div>
+            </div>			
+        </div>
+    </body>
 </html>
