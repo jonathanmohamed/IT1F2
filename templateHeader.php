838ed0c3f759cbc8ef33c7618594ac06666145a6
Thomas Koops <thomas_koops@hotmail.com>
2019-04-17 12:31:30 +0200

    Fixed header

--- templateHeader.php
+++ templateHeader.php
@@ -38,18 +38,29 @@
                 <div class="logo">
                     <a href="index.php"><img src="images/logo.jpg" alt="Logo" class="logo-img"/></a>
                 </div>
-                <div class="buttonlist1">
+                <div class="buttonlist1 en">
                     <a href="news.php" class="button"><h3 class="en">News</h3></a>
                     <a href="studStory.php" class="button"><h3 class="en">Student Stories</h3></a>
                     <a href="contact.php" class="button"><h3 class="en">Contact</h3></a>
-                    <a href="news.php" class="button"><h3 class="nl">News</h3></a>
-                    <a href="studStory.php" class="button"><h3 class="nl">Student Stories</h3></a>
-                    <a href="contact.php" class="button"><h3 class="nl">Contact</h3></a>
                     <div class="button">
                         <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
                             <select name="language">
                                 <option value="en" selected="selected">EN</option>
                                 <option value="nl">NL</option>
+                            </select>
+                            <input type="submit" name="submit" value="submit"/>
+                        </form>
+                    </div>
+                </div>
+                <div class="buttonlist1 nl">
+                    <a href="news.php" class="button"><h3>Nieuws</h3></a>
+                    <a href="studStory.php" class="button"><h3>Belevingen Studenten</h3></a>
+                    <a href="contact.php" class="button"><h3>Contact</h3></a>
+                    <div class="button">
+                        <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
+                            <select name="language">
+                                <option value="en">EN</option>
+                                <option value="nl" selected="selected">NL</option>
                             </select>
                             <input type="submit" name="submit" value="submit"/>
                         </form>
