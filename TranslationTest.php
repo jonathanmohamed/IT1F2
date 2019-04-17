0bfe214c5806093d10c4103c6b50f70a0790378c
Thomas Koops <thomas_koops@hotmail.com>
2019-04-17 10:22:46 +0200

    Added translation template and removed old files

---
+++ TranslationTest.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Translation</title>
+        <meta charset="UTF-8">
+        <link href="LanguageStyle.php" type="text/css" rel="stylesheet"/>
+        <style>
+            <?php
+                if(isset($_POST['submit'])){
+                    $language = $_POST['Language'];
+                }else{
+                    $language = "en";
+                }
+                
+            ?>
+            .nl
+            {
+                <?php
+                if($language == "en"){
+                    echo'display:none;'
+                    . 'visibility: hidden;';
+                }
+                ?>
+            }   
+            .en
+            {
+                <?php
+                if($language == "nl"){
+                    echo'display:none;'
+                    . 'visibility: hidden;';
+                }
+                ?>
+            }  
+        </style>
+    </head>
+    <body>
+        <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
+            <select name="Language">
+                <option value="en" selected="selected">EN</option>
+                <option value="nl">NL</option>
+            </select>
+            <input type="submit" name="submit" value="submit"/>
+        </form>
+        <div class="en">English section</div>
+        <div class="nl">Dutch section</div>
+    </body>
+</html>
